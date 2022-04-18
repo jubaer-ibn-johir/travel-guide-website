c382a11ab31fd3c9d9bc6b2445d1038ad6f3aebe
unknown <jubaeribnzohir@gmail.com>
2022-04-18 23:23:24 +0600

    all completed

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import jubaer from '../../images/me/jubaer.jpg' 
+import jubaer1 from '../../images/me/jubaer.jpg' 
 
 const About = () => {
     return (
@@ -7,7 +7,7 @@ const About = () => {
             
             <div>
                 <h2>Name: Jubaer Ibn Zohir</h2>
-                <img className='w-25' src={jubaer} alt="" />
+                <img className='w-25' src={jubaer1} alt="" />
                 <p>Assalamu Alaikum, Right now im studing in Jagannath University. I have some goals. i want to work for my country. Im working on it. But now i have a vision about programming. I know it can call day dream but i believe one day ill work on facebook as developer. Im struggling to achieve this goal. InshaAllah one day ill reach my goal </p>
             </div>
         </div>
